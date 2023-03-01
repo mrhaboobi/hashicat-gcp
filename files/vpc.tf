22adcd75f3872cc595e867a48aa80a930b38a7bb
Greg Cook <greg.cook@trademe.co.nz>
2023-03-01 23:17:46 +0000

    Added vpc module 4

--- files/vpc.tf
+++ files/vpc.tf
@@ -1,16 +1,2 @@
-module "network" {
-  source  = "https://github.com/mrhaboobi/terraform-google-network"
-  version = "3.4.0"
-  network_name = "GauravLand"
-  project_id = var.project
-  
-  subnets = [
-        {
-            subnet_name   = "gaurav-subnet"
-            subnet_ip     = "10.100.10.0/24"
-            subnet_region = var.region
-        }
-    ]
 
-}
 
---
+++ vpc.tf
@@ -0,0 +1,15 @@
+module "network" {
+  source  = "app.terraform.io/Hashi-Corp-Training/network/google"
+  version = "3.4.0"
+  network_name = "GauravLand"
+  project_id = var.project
+  
+  subnets = [
+        {
+            subnet_name   = "gaurav-subnet"
+            subnet_ip     = "10.100.10.0/24"
+            subnet_region = var.region
+        }
+    ]
+
+}
